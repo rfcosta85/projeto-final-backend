22d73c924a620faad00c78d84fd0cd67e4d6c3b6
unknown <rfcosta85@gmail.com>
2022-04-10 16:10:13 -0300

    postControllers and userControllers

--- projetoFinal/back-end/src/controllers/user-controller.js
+++ projetoFinal/back-end/src/controllers/user-controller.js
@@ -11,3 +11,25 @@ export const index = async (req, res) => {
     res.status(500).send({ error: `Cannot fetch users` });
   }
 };
+
+export const getUser = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    let user = await prisma.user.findFirst({
+      select: {
+        name: true,
+        username: true,
+        email: true,
+        createAt: true,
+      },
+      where: {
+        id: parseInt(id),
+      },
+    });
+    return res.send({ user });
+  } catch (error) {
+    console.error("user", error);
+    res.status(500).send({ error: `Cannot fetch user` });
+  }
+};
